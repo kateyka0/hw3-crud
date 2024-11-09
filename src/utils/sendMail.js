4b9571eaf0c5546825825a2d14dd4f4743838956
Katrin <kateyka1410@gmail.com>
2024-11-09 18:06:51 +0200

    update3

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -1,14 +1,14 @@
 import nodemailer from 'nodemailer';
 
-import { SMTP } from '../constants/index.js';
+
 import { env } from '../env.js';
 
 const transporter = nodemailer.createTransport({
-  host: env(SMTP.SMTP_HOST),
-  port: Number(env(SMTP.SMTP_PORT)),
+  host: env('SMTP_HOST'),
+  port: Number(env('SMTP_PORT')),
   auth: {
-    user: env(SMTP.SMTP_USER),
-    pass: env(SMTP.SMTP_PASSWORD),
+    user: env('SMTP_USER'),
+    pass: env('SMTP_PASSWORD'),
   },
 });
 
