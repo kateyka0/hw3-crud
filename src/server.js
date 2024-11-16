dbdb6295fbd5fd0b90b916fc9cf64af044080997
Katrin <kateyka1410@gmail.com>
2024-11-16 20:39:42 +0200

    update and create

--- src/server.js
+++ src/server.js
@@ -7,30 +7,27 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { sendEmailError } from './middlewares/sendEmailError.js';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import path from 'path';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
-  app.use(
+app.use('/api-docs', swaggerDocs());
+ 
+app.use(
     pino({
       transport: {
         target: 'pino-pretty',
       },
     }),
   );
-
-  const swaggerDocument = YAML.load(path.join(__dirname, '../docs/openapi.yaml'));
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  
 
   app.use(router);
 
